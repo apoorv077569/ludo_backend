f445628ea208038d3f3e405710b8ff15fb423794
Apoorv Rathore <apoorv@bitmaxgroup.com>
2025-09-02 18:25:27 +0530

    Update roomSocket.js

--- src/routes/roomRoutes.js
+++ src/routes/roomRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createRoom,joinRoom,deleteRoom,getRooms } from "../controllers/roomController.js";
+import { createRoom,joinRoom,deleteRoom,getRooms,getRoomByRoomId } from "../controllers/roomController.js";
 
 const roomRouter = express.Router();
 
@@ -8,4 +8,6 @@ roomRouter.post("/create", createRoom);
 roomRouter.get("/",getRooms);          // get all roomsq
 roomRouter.put("/join/:id",joinRoom); // join room
 roomRouter.delete("/delete/:id",deleteRoom); // delete room
+roomRouter.get("/:id", getRoomByRoomId); // get room by id
+
 export default roomRouter;
