62ef852ba7b816fb47bed74e67588441c5d1a57c
Apoorv Rathore <apoorv@bitmaxgroup.com>
2025-09-02 18:00:01 +0530

    Add files via upload

--- src/routes/roomRoutes.js
+++ src/routes/roomRoutes.js
@@ -8,4 +8,6 @@ roomRouter.post("/create", createRoom);
 roomRouter.get("/",getRooms);          // get all roomsq
 roomRouter.put("/join/:id",joinRoom); // join room
 roomRouter.delete("/delete/:id",deleteRoom); // delete room
+roomRouter.get("/:id", getRoomById); // get room by id
+
 export default roomRouter;
